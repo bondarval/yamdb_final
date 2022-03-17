57d1a0e0395c51505ab302c4c436a6060abb10d1
bondarval <bondamaker@gmail.com>
2022-03-17 10:09:11 +0500

    more fixes in fourth version of sprint16 homework

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -59,8 +59,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
-            'DB_ENGINE', default='django.db.backends.postgresql'
-            ),
+            'DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
